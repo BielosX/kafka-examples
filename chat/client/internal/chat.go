f4e44f776bfbd3b01dd68b99ff1a211eba8f1543
Mateusz Biel <matbiel@gmail.com>
2026-02-02 17:51:10 +0100

    Implement TUI client

---
+++ chat/client/internal/chat.go
@@ -0,0 +1,286 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"protobuf/gen"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/coder/websocket"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	ConnectionInputState = iota
+	ChatState
+)
+
+type ChatModel struct {
+	connection   *websocket.Conn
+	focusIndex   int
+	inputs       []textinput.Model
+	viewport     viewport.Model
+	chatInput    textarea.Model
+	cursorMode   cursor.Mode
+	state        int
+	err          error
+	msgChan      chan *messages.ServerResponse
+	errChan      chan error
+	readerCancel context.CancelFunc
+	messages     []string
+}
+
+var (
+	focusedStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	noStyle       = lipgloss.NewStyle()
+	blurredStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	errorStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#ff0000"))
+	focusedButton = focusedStyle.Render("[ Connect ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Connect"))
+)
+
+func NewChatModel() *ChatModel {
+	chatInput := textarea.New()
+	chatInput.SetWidth(30)
+	chatInput.SetHeight(3)
+	chatInput.KeyMap.InsertNewline.SetEnabled(false)
+	chatInput.ShowLineNumbers = false
+	chatInput.Focus()
+	model := &ChatModel{
+		state:     ConnectionInputState,
+		inputs:    make([]textinput.Model, 3),
+		viewport:  viewport.New(30, 5),
+		chatInput: chatInput,
+		msgChan:   make(chan *messages.ServerResponse, 1024),
+		errChan:   make(chan error),
+	}
+	for i := range model.inputs {
+		t := textinput.New()
+		t.PromptStyle = noStyle
+		t.TextStyle = noStyle
+		t.Cursor.Style = noStyle
+		t.Width = 256
+		t.CharLimit = 32
+		switch i {
+		case 0:
+			t.Placeholder = "Address"
+			t.CharLimit = 256
+			t.TextStyle = noStyle
+			t.PromptStyle = focusedStyle
+			t.Cursor.Style = focusedStyle
+			t.Focus()
+		case 1:
+			t.Placeholder = "Room"
+		case 2:
+			t.Placeholder = "Nick"
+		}
+		model.inputs[i] = t
+	}
+	return model
+}
+
+func (m *ChatModel) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func mod(a, b int) int {
+	return ((a % b) + b) % b
+}
+
+type ConnectionResult struct {
+	Conn  *websocket.Conn
+	Error error
+}
+
+func (m *ChatModel) connect() tea.Msg {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	address := m.inputs[0].Value()
+	room := m.inputs[1].Value()
+	nick := m.inputs[2].Value()
+	url := fmt.Sprintf("%s?nick=%s&room=%s", address, nick, room)
+	c, _, err := websocket.Dial(ctx, url, nil)
+	return ConnectionResult{
+		Conn:  c,
+		Error: err,
+	}
+}
+
+func (m *ChatModel) readMessages(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			_, msg, err := m.connection.Read(ctx)
+			if err != nil {
+				m.errChan <- err
+			}
+			var response messages.ServerResponse
+			err = proto.Unmarshal(msg, &response)
+			if err != nil {
+				m.errChan <- err
+			}
+			m.msgChan <- &response
+		}
+	}
+}
+
+type TickMsg time.Time
+
+func doTick() tea.Cmd {
+	return tea.Tick(time.Millisecond*200, func(t time.Time) tea.Msg {
+		return TickMsg(t)
+	})
+}
+
+func (m *ChatModel) handleTick() (tea.Model, tea.Cmd) {
+	select {
+	case err := <-m.errChan:
+		m.err = err
+	default:
+		// noop
+	}
+	for i := 0; i < 50; i++ {
+		select {
+		case msg := <-m.msgChan:
+			m.messages = append(m.messages, msg.GetPayload().GetContent().GetMessage())
+		default:
+			// noop
+		}
+	}
+	m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+	return m, doTick()
+}
+
+func (m *ChatModel) updateConnectionInput(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case ConnectionResult:
+		if msg.Error != nil {
+			m.err = msg.Error
+		} else {
+			m.state = ChatState
+			m.connection = msg.Conn
+			ctx, cancel := context.WithCancel(context.Background())
+			m.readerCancel = cancel
+			go m.readMessages(ctx)
+			return m, doTick()
+		}
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter:
+			if m.focusIndex == 3 {
+				return m, m.connect
+			}
+		case tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyUp, tea.KeyDown:
+			if msg.Type == tea.KeyUp {
+				m.focusIndex = mod(m.focusIndex-1, 4)
+			} else {
+				m.focusIndex = (m.focusIndex + 1) % 4
+			}
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := range m.inputs {
+				if i == m.focusIndex {
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					m.inputs[i].Cursor.Style = focusedStyle
+					continue
+				}
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+			return m, tea.Batch(cmds...)
+		default:
+			break
+		}
+	}
+	cmds := make([]tea.Cmd, len(m.inputs))
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+	return m, tea.Batch(cmds...)
+}
+
+func (m *ChatModel) writeMessage(msg string) error {
+	builder := messages.MessageContent_builder{}
+	builder.Severity = messages.MessageSeverity_NORMAL.Enum()
+	builder.Message = proto.String(msg)
+	content := builder.Build()
+	payload, err := proto.Marshal(content)
+	if err != nil {
+		return err
+	}
+	err = m.connection.Write(context.Background(), websocket.MessageBinary, payload)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *ChatModel) updateChat(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+	m.chatInput, tiCmd = m.chatInput.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	switch msg := msg.(type) {
+	case TickMsg:
+		return m.handleTick()
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.err = m.writeMessage(m.chatInput.Value())
+			m.chatInput.Reset()
+		}
+	}
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m *ChatModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch m.state {
+	case ConnectionInputState:
+		return m.updateConnectionInput(msg)
+	case ChatState:
+		return m.updateChat(msg)
+	default:
+		return m, nil
+	}
+}
+
+func (m *ChatModel) View() string {
+	var builder strings.Builder
+
+	switch m.state {
+	case ConnectionInputState:
+		for i := range m.inputs {
+			builder.WriteString(m.inputs[i].View())
+			builder.WriteRune('\n')
+		}
+		button := &blurredButton
+		if m.focusIndex == 3 {
+			button = &focusedButton
+		}
+		if m.err != nil {
+			_, _ = fmt.Fprintf(&builder, "\n%s\n%s\n", *button, errorStyle.Render(m.err.Error()))
+		} else {
+			_, _ = fmt.Fprintf(&builder, "\n%s\n\n", *button)
+		}
+	case ChatState:
+		_, _ = fmt.Fprintf(&builder, "%s\n\n%s\n", m.viewport.View(), m.chatInput.View())
+	}
+	return builder.String()
+}
--- chat/client/main.go
+++ chat/client/main.go
@@ -1,71 +1,18 @@
 package main
 
 import (
-	"context"
-	"flag"
+	"client/internal"
 	"fmt"
-	"protobuf/gen"
-	"time"
+	"os"
 
-	"github.com/coder/websocket"
-	"google.golang.org/protobuf/proto"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func closeNow(c *websocket.Conn) {
-	err := c.CloseNow()
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
 func main() {
-	var host string
-	var room string
-	var nick string
-	flag.StringVar(&host, "host", "", "Server address")
-	flag.StringVar(&room, "room", "", "Chat room")
-	flag.StringVar(&nick, "nick", "", "Chat nick")
-	flag.Parse()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-	url := fmt.Sprintf("%s?nick=%s&room=%s", host, nick, room)
-	//noinspection GoResourceLeak
-	c, _, err := websocket.Dial(ctx, url, nil)
+	model := internal.NewChatModel()
+	_, err := tea.NewProgram(model).Run()
 	if err != nil {
-		panic(err.Error())
-	}
-	defer closeNow(c)
-	var line string
-	_, err = fmt.Scanln(&line)
-	if err != nil {
-		panic(err.Error())
-	}
-	messageBuilder := messages.MessageContent_builder{}
-	messageBuilder.Message = proto.String(line)
-	messageBuilder.Severity = messages.MessageSeverity_NORMAL.Enum()
-	bytes, err := proto.Marshal(messageBuilder.Build())
-	if err != nil {
-		panic(err.Error())
-	}
-	err = c.Write(ctx, websocket.MessageBinary, bytes)
-	if err != nil {
-		panic(err.Error())
-	}
-	_, resp, err := c.Read(ctx)
-	if err != nil {
-		panic(err.Error())
-	}
-	var serverResponse messages.ServerResponse
-	err = proto.Unmarshal(resp, &serverResponse)
-	if err != nil {
-		panic(err.Error())
-	}
-	message := serverResponse.GetPayload().GetContent().GetMessage()
-	from := serverResponse.GetPayload().GetFrom()
-	timestamp := serverResponse.GetTimestamp()
-	fmt.Printf("[%s]%s: %s", timestamp.AsTime().Format(time.RFC3339), from, message)
-	err = c.Close(websocket.StatusNormalClosure, "")
-	if err != nil {
-		panic(err.Error())
+		_, _ = fmt.Fprintf(os.Stderr, "Could not start ChatModel, error: %v", err)
+		os.Exit(1)
 	}
 }
